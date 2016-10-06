746eae95b4afd1dc7fa467a0ba94b0631e19e4c3
ajaybhatia <prof.ajaybhatia@gmail.com>
2016-10-06 21:23:52 +0530

    Here I complete the full logic

--- src/odm_builder/views.py
+++ src/odm_builder/views.py
@@ -1,9 +1,15 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
+from django.conf import settings
+
 from .forms import ODMForm
 from .models import Build
 
-from subprocess import call
+from subprocess import call, Popen, PIPE
+from os.path import dirname, join, exists
+
+# Use 12factor inspired environment variables or from a file
+import environ
 
 @login_required
 def index(request):
@@ -14,18 +20,41 @@ def index(request):
 			build.user = request.user
 			
 			# Main Logic goes here
-			build_file_name = build.target_file
-			target_file_name = build.buildprop_file
-			'''
-			export target_file_name="p7201-target_files-1474857792.zip"
-			export mmx_key_path="/home/gursimran/YUClosed/vendor/yuos/security/yuos_keys"
-			export path_of_target_file="/home/gursimran/ODM/tinno/dist"
-			export new_path_of_signed_target_files="/home/gursimran/YUClosed"
-			export OS_Android_version="MM-6.0.1"
-			export Build_ID="MMB30K"
-			export mmx_build_version="MMXMR1.0"
-			export target_product="jalebi"
-			'''
+			build_file_name = build.target_file.name
+			target_file_name = build.buildprop_file.name
+			
+			env = environ.Env()
+			env_file = join(dirname(__file__), 'config.env')
+			if exists(env_file):
+			    environ.Env.read_env(str(env_file))
+
+			MMX_KEY_PATH = env('MMX_KEY_PATH')
+			PATH_OF_TARGET_FILE = env('PATH_OF_TARGET_FILE')
+			NEW_PATH_OF_SIGNED_TARGET_FILES = env('NEW_PATH_OF_SIGNED_TARGET_FILES')
+			
+			MEDIA_PATH = settings.MEDIA_ROOT
+			build_file = join(MEDIA_PATH, build_file_name)
+			sign_file = join(MEDIA_PATH, 'Sign.sh')
+
+			import re
+			build_str = open(build_file, "r")
+			for line in build_str:
+				if re.match("ro.build.id", line):
+					build_id = line.split('=')[1]
+				if re.match("ro.build.fingerprint", line):
+					build_version = line.split('=')[1].split('/')[4].split(':')[0]
+				if re.match("ro.yu.device", line):
+					target_product = line.split('=')[1]
+
+			call('export target_file_name="' + build_file_name + '"', shell=True)
+			call('export mmx_key_path="' + MMX_KEY_PATH + '"', shell=True)
+			call('export path_of_target_file="' + PATH_OF_TARGET_FILE + '"', shell=True)
+			call('export new_path_of_signed_target_files="' + NEW_PATH_OF_SIGNED_TARGET_FILES + '"', shell=True)
+			call('export OS_Android_version="' + build.android_version + '"', shell=True)
+			call('export Build_ID="' + build_id + '"', shell=True)
+			call('export mmx_build_version="' + build_version + '"', shell=True)
+			call('export target_product="' + target_product + '"', shell=True)
+			call('./' + sign_file + ' ' + build.build_type, shell=True)
 
 			build.save()
 			return redirect('/builder/home/')
