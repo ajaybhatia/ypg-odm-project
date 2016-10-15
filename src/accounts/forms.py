504e98bbad240281ba97dcb1aff51bfeb03dfe9d
ajaybhatia <prof.ajaybhatia@gmail.com>
2016-10-15 12:16:38 +0530

    Temp. enabling signup. This is causing an issue.

--- src/accounts/forms.py
+++ src/accounts/forms.py
@@ -28,20 +28,20 @@ class LoginForm(AuthenticationForm):
             )
 
 
-# class SignupForm(authtoolsforms.UserCreationForm):
+class SignupForm(authtoolsforms.UserCreationForm):
 
-#     def __init__(self, *args, **kwargs):
-#         super(SignupForm, self).__init__(*args, **kwargs)
-#         self.helper = FormHelper()
-#         self.fields["email"].widget.input_type = "email"  # ugly hack
+    def __init__(self, *args, **kwargs):
+        super(SignupForm, self).__init__(*args, **kwargs)
+        self.helper = FormHelper()
+        self.fields["email"].widget.input_type = "email"  # ugly hack
 
-#         self.helper.layout = Layout(
-#             Field('email', placeholder="Enter Email", autofocus=""),
-#             Field('name', placeholder="Enter Full Name"),
-#             Field('password1', placeholder="Enter Password"),
-#             Field('password2', placeholder="Re-enter Password"),
-#             Submit('sign_up', 'Sign up', css_class="btn-warning"),
-#             )
+        self.helper.layout = Layout(
+            Field('email', placeholder="Enter Email", autofocus=""),
+            Field('name', placeholder="Enter Full Name"),
+            Field('password1', placeholder="Enter Password"),
+            Field('password2', placeholder="Re-enter Password"),
+            Submit('sign_up', 'Sign up', css_class="btn-warning"),
+            )
 
 
 class PasswordChangeForm(authforms.PasswordChangeForm):
